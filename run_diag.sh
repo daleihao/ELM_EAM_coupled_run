36bd4deee0c14b54c604999d086ae5a27c66b685
Dalei Hao <dalei.hao.93@gmail.com>
2022-11-15 12:52:43 -0800

    Update run_diag.sh

--- run_diag.sh
+++ run_diag.sh
@@ -1,9 +1,10 @@
-#!/bin/bash -l
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -t 06:00:00
+#SBATCH --time-min=06:00:00 
+#SBATCH -q regular
+#SBATCH --account=ESMD
 #SBATCH --job-name=diags
-#SBATCH --output=diags.o%j
-#SBATCH --account=e3sm
-#SBATCH --nodes=1
-#SBATCH --time=01:00:00
 
 source /share/apps/E3SM/conda_envs/load_latest_e3sm_unified_compy.sh
 python run_e3sm_diags.py --multiprocessing --num_workers=32
